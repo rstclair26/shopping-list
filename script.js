5003bd7564d23cd250dd7c9c4c0cdb783ba7c5fd
Ross St. Clair <ross.stclair@gmail.com>
2025-05-15 17:42:19 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+
+function addItem(e) {
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+
+    // Validate input
+    if (newItem === "") {
+        alert("Please enter an item");
+        return;
+    }
+
+    // Create list item
+    const listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(newItem));
+
+    const button = createButton("remove-item btn-link text-red");
+    listItem.appendChild(button);
+
+    itemList.appendChild(listItem);
+
+    itemInput.value = "";
+}
+
+function createButton(classes) {
+    const button = document.createElement("button");
+    button.className = classes;
+
+    const icon = createIcon("fa-solid fa-xmark");
+    button.appendChild(icon);
+
+    return button;
+}
+
+function createIcon(classes) {
+    const icon = document.createElement("i");
+    icon.className = classes;
+    return icon;
+}
+
+// Event listeners
+itemForm.addEventListener("submit", addItem);
